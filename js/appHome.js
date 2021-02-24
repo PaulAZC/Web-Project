9435508404224bb96f7ee5f3cf8f9b8ca99a633b
PaulAZC <ayzacp@gmail.com>
2021-02-24 18:32:52 +0000

    Little animation of Typewrite
    Need to add more information and to add other pages with good effecs + nav when we scroll

--- js/appHome.js
+++ js/appHome.js
@@ -1,6 +1,15 @@
 const button = document.querySelectorAll('.discoverBtn');
+const textSection1 = document.getElementById('titleFirstSection');
 
 window.addEventListener('load', () => {
+    new Typewriter(textSection1, {
+    })
+    .changeDelay(80)
+    .typeString('Welcome to my site, ')
+    .pauseFor(1000)
+    .typeString('that talk about <span style="color:red">that</span>')
+    .start();
+
     const TL = gsap.timeline({pause:true});
     TL.staggerFrom(button, 2, {opacity:0, ease:"power2.out"}, 0.3);
     TL.play();
