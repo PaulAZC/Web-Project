0f694e7e791b1452ad265dca8d8a1ed1d8ff2a9d
PaulAZC <ayzacp@gmail.com>
2021-03-01 10:34:47 +0000

    Footer + transition ? + test on ourProject
    To do : JSON on ourProject (image gallery) + carousel + contacts
    Bugs : transitions between pages, problems with the load and scroll event

--- js/appHome.js
+++ js/appHome.js
@@ -2,12 +2,45 @@ const button = document.querySelector('.discoverBtn');
 const textSection1 = document.getElementById('titleFirstSection');
 const load = document.querySelector('.load');
 const cube = document.querySelector('.cube');
+const backgroundFisrt = document.querySelector('.section1');
+const media = document.querySelectorAll('.socialList');
+const allBandes = document.querySelectorAll('.bande');
+const TLAnim = new TimelineMax();
+
+// function delay(n) {
+//     return new Promise((done) => {
+//       setTimeout(() => {
+//         done();
+//       }, n)
+//     })
+//   }
+  
+//   barba.init({
+  
+//     sync: true,
+  
+//     transitions: [
+//       {
+//         async leave(){
+//           const done = this.async();
+//           TLAnim
+//           .to(allBandes, {height: '100%', stagger: 0.05})  
+//           await delay(1500);
+//           done();
+  
+//         },
+//         enter(){
+//           TLAnim
+//           .to(allBandes, {height: '0%', stagger: 0.05})
+//         }
+//       }
+//     ]
+//   });
 
 window.addEventListener('load', () => {
-    load.classList.add('fondu-out');
-
     new Typewriter(textSection1, {
     })
+    .pauseFor(2000)
     .changeDelay(80)
     .typeString('Welcome to my site, ')
     .pauseFor(1000)
@@ -15,14 +48,20 @@ window.addEventListener('load', () => {
     .pauseFor(1000) 
     .typeString('<span style="color:red">that</span>')
     .start();
+
+    backgroundFisrt.classList.add('anim-back-first');
 });
 
-window.addEventListener('scroll', ()=>{
-    if(window.scrollY>70){
+window.addEventListener('scroll', ()=>{    
+    if(window.scrollY>180){
         button.classList.add('anim-button');
     }
 
     if(window.scrollY>500){
         cube.classList.add('anim-cube');
     }
+    if(window.scrollY>600){
+        media.classList.add('anim-media');
+    }
 });
+
