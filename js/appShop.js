6a5b46144244128766aa10c339ad9cf6394285ff
PaulAZC <ayzacp@gmail.com>
2021-03-23 01:36:47 +0000

    Shop design + cards
    Color of the design + shop card
    Need to do : put all in the carousel, need to choose the type of carousel

---
+++ js/appShop.js
@@ -0,0 +1,23 @@
+let header = document.querySelector('header');
+let fontHeader = document.querySelectorAll('header a');
+
+//Animation with the scroll
+window.addEventListener('scroll', ()=>{
+    console.log(window.scrollY);
+
+    if(window.scrollY<152){
+        header.style.setProperty("--main-bg-color", '#f1f1f1');
+        header.style.backgroundColor = "transparent";
+        for(let i=0; i<fontHeader.length; i++){
+            fontHeader[i].style.color = "#f1f1f1";  
+        }
+    }
+
+    if(window.scrollY>152){
+        header.style.setProperty("--main-bg-color", '#f1f1f1');
+        header.style.backgroundColor = "#8435F2";
+        for(let i=0; i<fontHeader.length; i++){
+            fontHeader[i].style.color = "#f1f1f1";  
+        }
+    }
+});
