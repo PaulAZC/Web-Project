738db42e76d0d7a85ede8fbee6b7e07dc06114ec
PaulAZC <ayzacp@gmail.com>
2021-04-01 15:51:59 +0100

    Begin of the media querie + of the carousel JSON
    Need to do : more querie + end of the carousel

--- js/appShop.js
+++ js/appShop.js
@@ -22,12 +22,63 @@ window.addEventListener('scroll', ()=>{
 
 //Load the carousel with JSON
 document.addEventListener('DOMContentLoaded', ()=>{
+    var carousel_json = document.getElementById('carousel_json');
+
+    var carousel = document.createElement('div');
+    carousel.setAttribute('class', 'carousel');
+
+    var buttonPrev = document.createElement('a');
+    buttonPrev.setAttribute('class', 'prev');
+
+    var iconPrev = document.createElement('i');
+    iconPrev.setAttribute('class', 'fas fa-chevron-left');
+    buttonPrev.appendChild(iconPrev);
+
+    var buttonNext = document.createElement('a');
+    buttonNext.setAttribute('class', 'next');
+
+    var iconNext = document.createElement('i');
+    iconNext.setAttribute('class', 'fas fa-chevron-right');
+    buttonNext.appendChild(iconNext);
+
+    carousel_json.appendChild(buttonPrev);
+
     fetch("/json/carousel_shop.json").then(function(el){
         el.json().then(function(json){
             for(let i=0; i<json.length; i++){
+                var div_image = document.createElement('div');
+                div_image.setAttribute('class', 'item '+i);
+
+                var image = document.createElement('img');
+                image.setAttribute('src', json[i].url);
+                image.setAttribute('alt', json[i].alt);
+
+                var caption = document.createElement('h2');
+                caption.textContent = json[i].name;
+
+                div_image.appendChild(image);
+                div_image.appendChild(caption);
+
+                carousel.appendChild(div_image);
             }
+            carousel_json.appendChild(carousel);
+            carousel_json.appendChild(buttonNext);
+            setupCarousel(json);
         });
     });
+    function setupCarousel(json){
+        var imageWidth = 500;
+
+        buttonNext.addEventListener('click', ()=>{
+            for(let i=0; i<json.length; i++){
+                allImages[i].style.transform = 'translateX('+imageWidth+');'
+            }
+        });
+        
+        buttonPrev.addEventListener('click', ()=>{
+
+        });
+    };
 });
 
 // JavaScript Document
